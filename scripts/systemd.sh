8f3e3236d6a42d49d60d3abda440a83ca62e9c67
MykhailoVat <vatolkinmihajlo@gmail.com>
2026-05-08 16:34:37 +0300

    fix docker_conf path in sysd script

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -4,7 +4,7 @@ set -e
 mkdir /etc/systemd/system/mywebapp
 #cp mywebapp.service /etc/systemd/system/
 #cp mywebapp.socket /etc/systemd/system/
-cp docker_conf/mywebapp.service /etc/systemd/system/
+cp ../docker_conf/mywebapp.service /etc/systemd/system/
 
 systemctl daemon-reload
 #systemctl start mywebapp.socket
